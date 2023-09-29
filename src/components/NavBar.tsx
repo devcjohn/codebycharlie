8173a0fff6fd010a6eb7767050a7051bd8ccf6e1
Chuck Johnson <devcjohn@gmail.com>
2023-09-29 14:12:14 -0500

    Remove dsupport for dark mode, since it  has issues and needs dedicated work.  I can implement it later

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,27 +5,21 @@ export const NavBar = () => {
   const dropDownOptions = (
     <div
       id="dropdownNavbar"
-      className="absolute top-full left-0 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 group-hover:block z-50"
+      className="absolute top-full left-0 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44  group-hover:block z-50"
     >
-      <ul
-        className="py-2 text-sm text-gray-700 dark:text-gray-400"
-        aria-labelledby="dropdownLargeButton"
-      >
+      <ul className="py-2 text-sm text-gray-700 " aria-labelledby="dropdownLargeButton">
         <li>
-          <Link
-            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600"
-            to="/whiteboard"
-          >
+          <Link className="block px-4 py-2 hover:bg-gray-100 " to="/whiteboard">
             Whiteboard
           </Link>
         </li>
         <li>
-          <Link className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600" to="/FormFill">
+          <Link className="block px-4 py-2 hover:bg-gray-100 " to="/FormFill">
             Form Fill
           </Link>
         </li>
         <li>
-          <Link className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600" to="/contact">
+          <Link className="block px-4 py-2 hover:bg-gray-100" to="/contact">
             Captcha
           </Link>
         </li>
@@ -38,7 +32,7 @@ export const NavBar = () => {
       <nav>
         <div className="max-w-screen flex flex-wrap items-center justify-evenly px-4">
           <a href="/" className="flex items-center">
-            <span className="my-5 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="my-5 self-center text-2xl font-semibold whitespace-nowrap">
               CodeByCharlie
             </span>
           </a>
@@ -47,7 +41,7 @@ export const NavBar = () => {
             <ul className="flex flex-col font-medium p-2 md:p-0  rounded-lg  md:flex-row md:space-x-8 md:mt-0 ">
               <li>
                 <Link
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                   to="/Posts"
                 >
                   Posts
@@ -58,7 +52,7 @@ export const NavBar = () => {
                 <button
                   id="dropdownNavbarLink"
                   data-dropdown-toggle="dropdownNavbar"
-                  className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                  className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto "
                 >
                   Demos{' '}
                   <svg /* Caret icon.  Rotates 180 when dropdown is open */
@@ -84,7 +78,7 @@ export const NavBar = () => {
               </li>
               <li>
                 <Link
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                   to="/WordGame"
                 >
                   Word Game
