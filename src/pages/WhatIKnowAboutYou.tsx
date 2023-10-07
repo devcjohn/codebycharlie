4a0ed2ed61112c8eb807a76058ac43a1a46b4836
Chuck Johnson <devcjohn@gmail.com>
2023-10-07 13:21:29 -0500

    Add to WIKAY page

--- src/pages/WhatIKnowAboutYou.tsx
+++ src/pages/WhatIKnowAboutYou.tsx
@@ -1,3 +1,20 @@
+const getIP = () => {
+  const peerConnection = new RTCPeerConnection({ iceServers: [] })
+  peerConnection.createDataChannel('')
+
+  peerConnection
+    .createOffer()
+    .then((offer) => peerConnection.setLocalDescription(offer))
+    .catch((error) => console.log(error))
+
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      return event.candidate.address
+    }
+  }
+  return null
+}
+
 export const WhatIKnowAboutYou = () => {
   const timing = window.performance.timing
   const timingKeys = Object.getOwnPropertyNames(Object.getPrototypeOf(timing))
@@ -9,15 +26,34 @@ export const WhatIKnowAboutYou = () => {
       if (!actualVal) {
         return null
       }
-      return { k, val: new Date(actualVal).getMilliseconds() }
+      const deltaMs = actualVal - window.performance.timeOrigin
+      const deltaDate = new Date(deltaMs)
+      return {
+        k,
+        raw: actualVal,
+        val: new Date(actualVal).getMilliseconds(),
+        deltaMs: deltaMs,
+        human: deltaDate.getSeconds() + '.' + deltaDate.getMilliseconds() + 's',
+      }
     })
     .filter((x) => x)
+    .sort((a, b) => a!.raw - b!.raw)
+
+  const timeToInteractive = perf.find((x) => x!.k === 'domInteractive')!.human
+  const timeToComplete = perf.find((x) => x!.k === 'domContentLoadedEventEnd')!.human
+  const performanceEntries = perf.map((x) => x!.k + ' : ' + x!.human)
 
   // Alternate way to get performance data keys/data
   //const performanceEntries = performance.getEntriesByType('navigation')
 
   const props = {
-    appName: navigator.appVersion,
+    ip: getIP(),
+    language: navigator.language,
+    Clipboard: navigator.clipboard.readText().then((x) => x),
+    languages: navigator.languages,
+    webDriver: navigator.webdriver,
+    timeToInteractive,
+    timeToComplete,
     glob: window.global,
     devicePixelRatio: window.devicePixelRatio,
     innerHeight: window.innerHeight,
@@ -29,31 +65,36 @@ export const WhatIKnowAboutYou = () => {
     cookieEnabled: navigator.cookieEnabled,
     doNotTrack: navigator.doNotTrack,
     hardwareConcurrency: navigator.hardwareConcurrency,
-    language: navigator.language,
-    languages: navigator.languages,
     maxTouchPoints: navigator.maxTouchPoints,
     onLine: navigator.onLine,
     pdfViewerEnabled: navigator.pdfViewerEnabled,
-    platform: navigator.platform,
-    product: navigator.product,
-    productSub: navigator.productSub,
     userAgent: navigator.userAgent,
-    userAgentData: navigator.userAgentData,
-    webDriver: navigator.webDriver,
-    GPUShaderStage: navigator.GPUShaderStage,
-    GPUTextureUsage: navigator.GPUTextureUsage,
-    performance: window.performance,
     activeElement: document.activeElement,
     isConnected: document.isConnected,
     lastModified: document.lastModified,
     readyState: document.readyState,
     referrer: document.referrer,
     currentTime: document.timeline.currentTime,
-    perf,
+    geo: navigator.geolocation.getCurrentPosition((x) => x),
+  }
+
+  const deprecatedOrExperimentalProps = {
+    appName: navigator.appVersion,
+    platform: navigator.platform,
+    product: navigator.product,
+    productSub: navigator.productSub,
+    performanceEntries,
+    userAgentData: navigator.userAgentData,
+    gpu: !navigator.gpu ? 'WebGPU not supported' : navigator.gpu,
+    ink: navigator.ink,
   }
   return (
     <>
+      <h1 className="text-4xl"> Props from your browser</h1>
       <pre>{JSON.stringify(props, null, 2)}</pre>
+      <h1 className="text-4xl"> Deprecated or experimental props from your browser</h1>
+      <h2 className="text-2xl">These may not work in all browsers</h2>
+      <pre>{JSON.stringify(deprecatedOrExperimentalProps, null, 2)}</pre>
     </>
   )
 }
