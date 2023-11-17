7d83d43f4e9e28edf8454a46e14bbe1c15281cdc
Chuck Johnson <devcjohn@gmail.com>
2023-11-17 12:25:11 -0600

    Bump react-markdown version and fix types .

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,11 +1,11 @@
+/* eslint-disable react/no-children-prop */
 import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/prism-light'
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import { CodeProps } from 'react-markdown/lib/ast-to-react'
-import { FC } from 'react'
+import { ClassAttributes, FC, HTMLAttributes } from 'react'
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx'
 import markdown from 'react-syntax-highlighter/dist/esm/languages/prism/markdown'
 import scheme from 'react-syntax-highlighter/dist/esm/languages/prism/scheme'
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown, { ExtraProps } from 'react-markdown'
 import { useLoaderData } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
 import { parseMarkdownWithYamlFrontmatter } from '../util/util'
@@ -16,23 +16,25 @@ SyntaxHighlighter.registerLanguage('jsx', jsx)
 SyntaxHighlighter.registerLanguage('markdown', markdown)
 SyntaxHighlighter.registerLanguage('scheme', scheme)
 
+type CodeBlockProps = ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps
+
 /* If code is detected, show code block with syntax highlighting */
-const CodeBlock: FC<CodeProps> = ({ className, children, inline, ...props }) => {
+const CodeBlock = (props: CodeBlockProps) => {
+  const { children, className, ...rest } = props
   const match = /language-(\w+)/.exec(className || '')
   const language = match ? match[1] : undefined /* eg 'xml', 'js', 'jsx', or 'markdown' */
-  return language ? (
+  return match ? (
     <SyntaxHighlighter
       language={language}
       PreTag="div"
-      {...props}
+      {...rest}
+      children={String(children).replace(/\n$/, '')}
       style={materialDark}
-      inline={inline}
       wrapLongLines={true}
-    >
-      {String(children).replace(/\n$/, '')} {/* Remove trailing newline */}
-    </SyntaxHighlighter>
+      ref={null} /* Override ref because the types are not compatible */
+    />
   ) : (
-    <code className={className} {...props}>
+    <code {...rest} className={className}>
       {children}
     </code>
   )
@@ -60,7 +62,7 @@ export const BlogPost: FC = () => {
       </Helmet>
       <div className="flex justify-center">
         <article className="prose prose-sm prose-slate m-4 md:prose-base lg:prose-lg prose-h2:underline md:m-20">
-          <ReactMarkdown components={{ code: CodeBlock }}>{content}</ReactMarkdown>
+          <ReactMarkdown children={content} components={{ code: (props) => CodeBlock(props) }} />
         </article>
       </div>
     </>
