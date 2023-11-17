e38248dc05ecd69c57156fd26a4adc62fbbf22ae
Chuck Johnson <devcjohn@gmail.com>
2023-11-17 11:31:02 -0600

    stashing these WIP loading components here

---
+++ src/components/Loading2.tsx
@@ -0,0 +1,121 @@
+import { useState } from 'react'
+
+export const Loading2 = () => {
+  const [count, setCount] = useState(1)
+  const [duration, setDuration] = useState(3)
+  const [animationTimingFunction, setAnimationTimingFunction] =
+    useState<AnimationTimingFunction>('linear')
+
+  //setAnimationTimingFunction('foo')
+
+  type AnimationTimingFunction =
+    | 'linear'
+    | 'ease'
+    | 'ease-in'
+    | 'ease-out'
+    | 'ease-in-out'
+    | 'step-start'
+    | 'step-end'
+    | 'steps()'
+    | 'cubic-bezier()'
+    | 'initial'
+    | 'inherit'
+
+  const mySVG = (
+    <svg
+      aria-hidden="true"
+      className="mr-8 h-8 w-8  fill-blue-600 text-gray-200 dark:text-gray-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 
+     0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 
+     27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 
+     75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501
+      6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735
+       17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      ></path>
+    </svg>
+  )
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={count}
+          onChange={(e) => setCount(parseInt(e.target.value))}
+          className="w-full"
+        />
+      </div>
+      <div>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={duration}
+          onChange={(e) => setDuration(parseInt(e.target.value))}
+          className="w-full"
+        />
+      </div>
+      {/* selector for AnimationTimingFunction (dropdown).  Options are  'linear' | 'ease' | 'ease-in', etc*/}
+      <select
+        onChange={(x) => setAnimationTimingFunction(x.target.value as AnimationTimingFunction)}
+      >
+        <option value="ease">ease</option>
+        <option value="linear">linear</option>
+        <option value="ease-in">ease-in</option>
+        <option value="ease-out">ease-out</option>
+        <option value="ease-in-out">ease-in-out</option>
+        {/* <option value="step-start">step-start</option>
+        <option value="step-end">step-end</option>
+        <option value="steps()">steps()</option>
+        <option value="cubic-bezier()">cubic-bezier()</option> */}
+      </select>
+
+      <div className="flex flex-wrap content-center justify-center items-center">
+        <div>
+          {new Array(count).fill(mySVG).map((svg, index) => {
+            // if (index % 2 === 0) {
+            const cssClass = `animate-[spin_${duration}s_ease-in-out_infinite_reverse]`
+            console.log({ cssClass })
+            return (
+              <div
+                key={index}
+                style={{
+                  // animation: `spin_${duration}s_ease-in-out_infinite_reverse`,
+                  animationName: 'spin',
+                  animationDuration: `${duration}s`,
+                  animationTimingFunction: animationTimingFunction,
+                  animationIterationCount: 'infinite',
+                  animationDirection: 'revert',
+                }}
+              >
+                {svg}
+              </div>
+            )
+            //   }
+            // return (
+            //   <div className="animate-[spin_3s_ease-in-out_infinite]" key={index}>
+            //     {svg}
+            //   </div>
+            // )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+{
+  /* <svg className="transition ease-in-out duration-150"></svg> */
+}
